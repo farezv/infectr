c7a0c4561b4484b82f195ce1a82fafb18d0fd225
Farez Vadsaria <farezvad@gmail.com>
2015-01-21 00:42:10 -0800

    Created coaches relations & infected students

--- Infectr.java
+++ Infectr.java
@@ -6,7 +6,8 @@ public class Infectr {
 	private static int CURRENT_VERSION = 1;
 
 	// In production, users would be in a database and deserialized into a User class upon request
-	private static HashMap<Integer, User> users;		
+	private static HashMap<Integer, User> users;
+	private static ArrayList<Integer> infected;		
 
 	public static void main(String[] args) {
 		if(args.length != 0) {
@@ -26,13 +27,15 @@ public class Infectr {
 					createUser("student", i);
 				}
 			}
+			infected = new ArrayList<Integer>();
 
 			infectAll(5); // only infects 5
 			printUsers();
-			createRelationships();
-			printRelationships();			
+			createCoachedByRelations();
+			createCoachesRelations();
+			printRelations();			
 			infectAll(7); // infects users starting from 7
-			printUsers();
+			System.out.println("Infected: " + String.valueOf(infected));
 
 		} else System.out.println("Please enter the number of users you'd like to create");
 	}
@@ -48,26 +51,42 @@ public class Infectr {
 		}
 	}
 
+	/* Recursively infects users */
 	public static void infectAll(int uid) {
 		if(!users.isEmpty()) {
 			User user = users.get(uid);
-			System.out.println("After getting user, before infecting it");
 			if(user != null && !user.isInfected(CURRENT_VERSION + 1)) {
 				// Infect the specifed user
 				user.setVersion(CURRENT_VERSION + 1);
+				infected.add(uid);
+				// Infect its coaches and students
 				for(int coachId: user.getCoaches()) {
 					infectAll(coachId);
+				}
+				for(int studentId: user.getStudents()) {
+					infectAll(studentId);
 				}
 			}	
 		}
 	}
 
-	public static void addRandomCoachAndStudent(int uid) {
+	/* Adds a random coach to the specified user */
+	public static void addRandomCoach(int uid) {
 		if(!users.isEmpty()) {
 			User user = users.get(uid);
 			if(user != null) {
 				user.addCoach(randomUidGenerator(uid));
-				user.addStudent(randomUidGenerator(uid));
+			}
+		}
+	}
+
+	public static void createCoachesRelations() {
+		for(User student: users.values()) {
+			for(int coachId: student.getCoaches()) {
+				User coach = users.get(coachId);
+				if(coach != null) {
+					coach.addStudent(student.getUid());
+				}
 			}
 		}
 	}
@@ -76,22 +95,22 @@ public class Infectr {
 	public static int randomUidGenerator(int uid) {
 		Random numGen = new Random();
 		int randomUid = numGen.nextInt(users.size());
-		// For good measure
+		// Just in case a user's own uid is randomly generated
 		while(randomUid == uid) {
 			randomUid = numGen.nextInt(users.size());
 		}
 		return randomUid;
 	}
 
-	public static void createRelationships() {
+	public static void createCoachedByRelations() {
 		for(int i = 0; i < users.size(); i++) {
-			addRandomCoachAndStudent(i);
+			addRandomCoach(i);
 		}
 	}
 
-	public static void printRelationships() {
+	public static void printRelations() {
 		for (User u: users.values()) {
-			System.out.println(String.valueOf(u.getUid()) + " coaches " + String.valueOf(u.getStudents()));
+			System.out.println(String.valueOf(u.getUid()) + " coaches " + String.valueOf(u.getStudents()) + " student of " + String.valueOf(u.getCoaches()));
 		}
 	}
 }
