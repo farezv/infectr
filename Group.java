3c552e3c739e1cda24954bedff76a16326fe61b9
Farez Vadsaria <farezvad@gmail.com>
2015-01-22 23:16:24 -0800

    Added head coach field and add user method

--- Group.java
+++ Group.java
@@ -1,16 +1,28 @@
+import java.lang.Integer;
+import java.util.*;
+
+/* A group is created with at least one coach & one student
+This restriction can of course be changed as needed */
 public class Group {
 
 	private String name;
-	private ArrayList<Integer> users;		
+	private ArrayList<Integer> users;	
+	private int headCoachId;	
 	
-	/* A group can contain any number of users is created from the first coach's id */
+	/* A group can contain any number of users and is created from the first coach's id */
 	public Group(int coachId) {
 		this.name = "Group " + String.valueOf(coachId);
 		this.users = new ArrayList<Integer>();
+		this.headCoachId = coachId;
 	}
 
+	/* Getters */
 	public String getName() {
 		return this.name;
+	}
+
+	public int getHeadCoachId() {
+		return this.headCoachId;
 	}
 
 	public ArrayList<Integer> getGroupUsers() {
@@ -21,4 +33,8 @@ public class Group {
 		return this.users.size();
 	}
 
+	/* Helpers */
+	public void addUser(int uid) {
+		this.users.add(uid);
+	}
 }
